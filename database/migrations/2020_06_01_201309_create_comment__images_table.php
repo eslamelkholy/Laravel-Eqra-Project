c5ee811e0903b5cee0c4272dc40f66fe58f9a578
mina <mina.george.powerful@gmail.com>
2020-06-02 16:19:06 +0200

    comments

---
+++ database/migrations/2020_06_01_201309_create_comment__images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment__images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('comment_id');
+            $table->binary('image');
+            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment__images');
+    }
+}
