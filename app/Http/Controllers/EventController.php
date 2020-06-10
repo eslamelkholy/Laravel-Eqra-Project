8c6dc336ffe7ec81de2eaca2378feaf03a4c3965
eslam <eslamelkholy5325762@gmail.com>
2020-06-10 18:35:24 +0200

    Event CRUD Operations & Validation

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Event;
 use App\Http\Resources\Event as EventResource;
+use App\Http\Requests\EventRequest;
 
 class EventController extends Controller
 {
@@ -14,23 +15,35 @@ class EventController extends Controller
         return EventResource::collection($events);
     }
 
-    public function store(Request $request)
+    public function store(EventRequest $request)
     {
-        //
+        $event = Event::create($request->all());
+        return response()->json($event, 201);
     }
 
     public function show($id)
     {
-        //
+        $event = Event::find($id);
+        if (is_null($event))
+            return response()->json(["message" => "Event Not Found"], 404);
+        return new EventResource($event);
     }
 
-    public function update(Request $request, $id)
+    public function update(EventRequest $request, $id)
     {
-        //
+        $event = Event::find($id);
+        if (is_null($event))
+            return response()->json(["message" => "Event Not Found"], 404);
+        $event->update($request->all());
+        return response()->json($event, 200);
     }
 
     public function destroy($id)
     {
-        //
+        $event = Event::find($id);
+        if (is_null($event))
+            return response()->json(["message" => "Post Not Found"], 404);
+        $event->delete();
+        return response()->json(null, 204);
     }
 }
--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -8,6 +8,7 @@ $factory->define(Event::class, function (Faker $faker)  use ($factory) {
     return [
         'name' => $faker->name,
         'description' => $faker->paragraph,
+        'location' => $faker->address,
         'start_date' => now(),
         'end_date'   => $faker->dateTimeBetween($startDate = 'now', $endDate = '30 days'),
         'user_id' => $faker->randomElement($userIds)
