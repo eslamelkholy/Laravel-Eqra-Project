2bcdb641cf1683174c85d0c70f90aee0fd32aa0f
Mahmoud <mahmoedmohamed19@gmail.com>
2020-06-13 20:20:30 +0200

    cleaning up api

--- routes/api.php
+++ routes/api.php
@@ -8,15 +8,11 @@ use Laravel\Passport\Bridge\AccessToken;
 use Laravel\Passport\Http\Controllers\AccessTokenController;
 
 // Authentication & User Api's
-Route::group([
-    'prefix' => 'auth'
-], function () {
+Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signup');
 
-    Route::group([
-        'middleware' => 'auth:api'
-    ], function () {
+    Route::group(['middleware' => 'auth:api'], function () {
         Route::get('logout', 'AuthController@logout');
         Route::get('user', 'AuthController@user');
         Route::put('users/edit', 'AuthController@update');
